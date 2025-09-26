b9ba8cce27b1d35097b834602ea16951c0252a26
ayuka-supra <ayukasupra@gmail.com>
2025-09-26 18:28:51 +1000

    added Javascript function to the single location

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  //paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
+  parisDateElement.innerHTML = parisTime.format("MMMM, Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //jakarta
+  let jakartaElement = document.querySelector("#jakarta");
+  let jakartaDateElement = jakartaElement.querySelector(".date");
+  let jakartaTimeElement = jakartaElement.querySelector(".time");
+  let jakartaTime = moment().tz("Asia/jakarta");
+
+  jakartaDateElement.innerHTML = jakartaTime.format("MMMM, Do YYYY");
+  jakartaTimeElement.innerHTML = jakartaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
